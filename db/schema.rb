3431d7e6fb6c3d36ad5fbfd2858a06657f6e0eb2
forison <forison6205@gmail.com>
2021-03-26 20:54:15 -0400

    Order model settings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_27_004530) do
+ActiveRecord::Schema.define(version: 2021_03_27_005231) do
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_orders_on_product_id"
+    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
@@ -27,4 +36,6 @@ ActiveRecord::Schema.define(version: 2021_03_27_004530) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "orders", "products"
+  add_foreign_key "orders", "users"
 end
