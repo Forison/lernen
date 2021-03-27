b6d1bfc2855dd85f6387216c8efe94fa3f05cfdb
forison <forison6205@gmail.com>
2021-03-26 20:49:18 -0400

    Add user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_27_003937) do
+ActiveRecord::Schema.define(version: 2021_03_27_004530) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.string "price"
     t.boolean "disabled"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "token"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
