f4adf4505e8bc7f263fa02610b953beae491188d
forison <forison6205@gmail.com>
2021-03-28 00:43:17 -0400

    Set up test env

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   resources :products
   resources :orders, only: [:create, :index, :show, :destroy]
   post '/checkout', to: "orders#checkout"
-  get'/checkout'
   root "products#index"
 end
